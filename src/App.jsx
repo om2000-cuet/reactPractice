95a60538bd431791750ee6b27844ff43b3bf8955
om2000-cuet <prakash02cuet@gmail.com>
2025-10-31 00:36:38 +0600

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1>Welcome to my Website</h1>
       <Props1 name="Sagar" age="41" location="USA" />
       <Props1 name="Bony" age="42" location="UK" />
-      <Props1 name="Jony" age="43" location="NY" />
+      <Props1 name="Jonathan" age="43" location="NY" />
     </div>
   );
 }
